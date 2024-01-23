e993ffd67163c2950a2295704d843ee27fc40a26
erdjeii <>
2024-01-23 18:01:39 +0600

    first commit

---
+++ javascrip.js
@@ -0,0 +1,10 @@
+let formbox = document.querySelector('.form')
+let popup = ()=>{
+formbox.classList.toggle('active')
+}
+
+let burger = document.querySelector('.burger')
+let toggleBurger = ()=>{
+    burger.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+}
